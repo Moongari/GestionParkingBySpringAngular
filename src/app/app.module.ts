3918e656cfba0b7d97f9fd92a8b065bef42870bb
moustafa.moungari <moungari.moustafa@hotmail.com>
2021-07-22 15:36:57 +0200

    ajout angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ParkingsComponent } from './parkings/parkings.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,10 @@ import { ParkingsComponent } from './parkings/parkings.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
